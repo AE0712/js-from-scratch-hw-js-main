45f31af9f743e1c0cb70e9539787dae39c8b7b37
Anatoliy Evseev <ea62441@gmail.com>
2026-05-23 22:01:38 +0300

    lesson04/04 complete

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,11 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let doubleStr = '';
+    for (let i = 0; i < str.length; i++) {
+         doubleStr += str[i] + str[i];
+    }
+    return doubleStr
+}
+console.log(doubleEachCharacter('hello'));
